4c2b65489c12488c6d047d6ce60e74fe58367a81
the_coding_robot <davidmiszczyk@gmail.com>
2018-12-21 22:04:02 -0600

    Add files via upload

---
+++ Vigenere Cipher.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 21 21:02:18 2018
+
+@author: david
+"""
+#Vigenere Cipher
+
+def Vigenere_Cipher(message,key):
+    """Vigenere Cipher using list methods in the code.Takes two strings as input."""
+    message=message.upper()
+    key=key.upper()
+    alphabet='abcdefghijklmnopqrstuvwxyz'.upper()
+    alpha=list(alphabet)
+    alpha=alpha+alpha
+    encryption=''
+    k_ls=[]
+    m_ls=[]
+
+    for letter in key:
+        letter_index=alpha.index(letter)
+        k_ls+=[letter_index+1]    
+        
+    for ch in message:
+        if ch not in alpha:
+            pass
+        else:
+            ch_index=alpha.index(ch)
+            m_ls+=[ch_index]
+            
+    while len(k_ls)<len(m_ls):  
+        k_ls+=k_ls
+    
+    while len(k_ls)>len(m_ls):
+        k_ls.pop()
+        
+    e_ls=[m_ls[i]+k_ls[i] for i in range(len(m_ls))]
+    
+    encrypt=[alpha[i] for i in e_ls]
+    
+    for i in encrypt:
+        encryption+=i
+        
+    return encryption
