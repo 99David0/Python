27b0a130aa38f6fa20019fb09882f5d6528819be
the_coding_robot <davidmiszczyk@gmail.com>
2018-12-22 21:36:25 -0600

    Update Caesar Cipher.py

--- Caesar Cipher.py
+++ Caesar Cipher.py
@@ -7,17 +7,17 @@ Created on Wed Sep 12 17:38:47 2018
 
 def Caesar_Cipher(message,key):
     """Takes two inputs, a string and an integer, and rotates the string by "key" values in the alphabet and returns the encryption. """
-    #string=string.upper()
+    
     message=message.upper()
     alphabet = 'abcdefghijklmnopqrstuvwxyz'.upper() 
-    ls_indices=[]   #Contains the index of each character in "message."
+    ls_indices=[]   
     encrypt=''
     
     for ch in message:
         if ch not in alphabet:
             pass
         else:
-            ch_indices=alphabet.find(ch)  #The find method returns the index of that character in the string.
+            ch_indices=alphabet.find(ch)  
             ls_indices+=[ch_indices] 
             shift=(ch_indices+key)%26
             encrypt+=alphabet[shift]
